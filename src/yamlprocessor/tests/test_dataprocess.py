4ad2f2e30c18b05b6e6fe9b2a7dc70ec62ec99fc
Matt Shin <matthew.shin@metoffice.gov.uk>
2022-01-28 09:49:42 +0000

    Schema validate and date time substitute
    Add logic to validate data against a JSON schema.
    
    Add logic to substitute variable-like syntax with calculated date-time.

--- src/yamlprocessor/tests/test_dataprocess.py
+++ src/yamlprocessor/tests/test_dataprocess.py
@@ -1,3 +1,6 @@
+import json
+
+from dateutil.parser import parse as datetimeparse
 import pytest
 import yaml
 
@@ -37,6 +40,59 @@ def test_process_variable_1():
         'Who is the unknown?'
         == processor.process_variable('Who is the ${ALIEN}?')
     )
+
+
+def test_process_variable_2():
+    """Test DataProcessor.process_variable time substitution syntax."""
+    processor = DataProcessor()
+    processor.variable_map.clear()
+    processor.time_now = datetimeparse('2012-01-20T12:01')
+    processor.time_ref = datetimeparse('2022-02-20T22:02')
+    assert (
+        processor.process_variable(r"${YP_TIME_NOW}")
+        == '2012-01-20T12:01:00')
+    assert (
+        processor.process_variable(r"${YP_TIME_REF}")
+        == '2022-02-20T22:02:00')
+    assert (
+        processor.process_variable(r"${YP_TIME_REF_AT_T0H0M0S}")
+        == '2022-02-20T00:00:00')
+    assert (
+        processor.process_variable(r"${YP_TIME_REF_AT_1DT0H0M0S}")
+        == '2022-02-01T00:00:00')
+    assert (
+        processor.process_variable(r"${YP_TIME_REF_MINUS_T10H2M}")
+        == '2022-02-20T12:00:00')
+    assert (
+        processor.process_variable(r"${YP_TIME_REF_PLUS_10D}")
+        == '2022-03-02T22:02:00')
+    assert (
+        processor.process_variable(r"${YP_TIME_REF_AT_1DT0H0M0S_MINUS_T12H}")
+        == '2022-01-31T12:00:00')
+
+
+def test_process_variable_3():
+    """Test DataProcessor.process_variable time substitution format."""
+    processor = DataProcessor()
+    processor.variable_map.clear()
+    processor.time_formats.update({
+        'ABBR': '%Y%m%dT%H%M%S%z',
+        'MIN_UTC': '%Y%m%dT%H%MZ',
+        'CTIME': '%a %e %b %T %Y',
+    })
+    processor.time_ref = datetimeparse('2022-02-20T22:02Z')
+    assert (
+        processor.process_variable(r"${YP_TIME_REF}")
+        == '2022-02-20T22:02:00+0000')
+    assert (
+        processor.process_variable(r"${YP_TIME_REF_FORMAT_ABBR}")
+        == '20220220T220200+0000')
+    assert (
+        processor.process_variable(r"${YP_TIME_REF_FORMAT_MIN_UTC}")
+        == '20220220T2202Z')
+    assert (
+        processor.process_variable(r"${YP_TIME_REF_FORMAT_CTIME}")
+        == 'Sun 20 Feb 22:02:00 2022')
 
 
 def test_main_0(tmp_path):
@@ -178,3 +234,40 @@ def test_main_9(tmp_path):
     outfilename = tmp_path / 'b.yaml'
     main([str(infilename), str(outfilename)])
     assert yaml.safe_load(outfilename.open()) == data
+
+
+def test_main_validate_1(tmp_path, capsys):
+    """Test main, YAML with JSON schema validation."""
+    schema = {
+        'type': 'object',
+        'properties': {'hello': {'type': 'string'}},
+        'required': ['hello'],
+        'additionalProperties': False,
+    }
+    schemafilename = tmp_path / 'hello.schema.json'
+    with schemafilename.open('w') as schemafile:
+        json.dump(schema, schemafile)
+    outfilename = tmp_path / 'b.yaml'
+    infilename = tmp_path / 'a.yaml'
+    # Schema specified as an absolute file system path
+    with infilename.open('w') as infile:
+        infile.write(f'#!{schemafilename}\n')
+        yaml.dump({'hello': 'earth'}, infile)
+    main([str(infilename), str(outfilename)])
+    captured = capsys.readouterr()
+    assert f'[INFO] ok {outfilename}' in captured.err.splitlines()
+    # Schema specified as a file:// URL
+    with infilename.open('w') as infile:
+        infile.write(f'#!file://{schemafilename}\n')
+        yaml.dump({'hello': 'earth'}, infile)
+    main([str(infilename), str(outfilename)])
+    captured = capsys.readouterr()
+    assert f'[INFO] ok {outfilename}' in captured.err.splitlines()
+    # Schema specified as a relative path, with schema prefix
+    with infilename.open('w') as infile:
+        infile.write('#!hello.schema.json\n')
+        yaml.dump({'hello': 'earth'}, infile)
+    schema_prefix = f'--schema-prefix=file://{tmp_path}/'
+    main([schema_prefix, str(infilename), str(outfilename)])
+    captured = capsys.readouterr()
+    assert f'[INFO] ok {outfilename}' in captured.err.splitlines()
