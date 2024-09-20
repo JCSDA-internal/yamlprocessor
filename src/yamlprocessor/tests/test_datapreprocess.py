59116b505319b57dca97b1549896225e21191c13
mikecooke77 <michael.c.cooke@metoffice.gov.uk>
2024-09-20 21:39:41 +0100

    Make the test work

--- src/yamlprocessor/tests/test_datapreprocess.py
+++ src/yamlprocessor/tests/test_datapreprocess.py
@@ -5,7 +5,7 @@ import filecmp
 import pytest
 from ruamel.yaml import YAML
 
-from ..datapreprocess import (
+from ..datapreprocessor import (
     DataPreProcessor, main)
 
 
@@ -13,46 +13,43 @@ from ..datapreprocess import (
 def yaml():
     return YAML(typ='safe', pure=True)
 
-def create_input_files(path):
-    # Write the content to a file named "a.yaml"
-    with open(path+"/a.yaml", 'w') as file:
-        file.write("DIRECT_INCLUDE=$FILE_PATH/b.yaml\n")
-        file.write("\n")
-        file.write("data:\n")
-        file.write("    brain: *banana\n")
-        file.write("    tel: *groups\n")
-    # Write the content to a file named "b.yaml"
-    with open(path+"/b.yaml", 'w') as file:
-        file.write("_:\n")
-        file.write("- &banana 1\n")
-        file.write("- &groups [4, 5, 6]\n")
-
-def create_comparison(path):
-    with open(path+"/reference_0.yaml", 'w') as file:
-        file.write("_:\n")
-        file.write("- &banana 1\n")
-        file.write("- &groups [4, 5, 6]\n")
-        file.write("\n")
-        file.write("data:\n")
-        file.write("    brain: *banana\n")
-        file.write("    tel: *groups\n")
-
-def compare_files(file1, file2):
-    with open(file1, 'r') as f1, open(file2, 'r') as f2:
-        content1 = f1.read()
-        content2 = f2.read()
-        
-    assert content1 == content2, f"Files {file1} and {file2} do not match."
-
 def test_main_0(tmp_path, yaml):
     """Test main, basic."""
-    # Create test files
-    create_input_files(tmp_path)
-    create_comparison(tmp_path)
+    yaml_0 = """
+DIRECT_INCLUDE=$FILE_PATH/aux.yaml
+
+data:
+    brain: *banana
+    tel: *groups
+"""
+    yaml_1 = """
+_:
+- &banana 1
+- &groups [4, 5, 6]
+"""
+    reference = """
+_:
+- &banana 1
+- &groups [4, 5, 6]
+
+data:
+    brain: *banana
+    tel: *groups
+"""
+    infilename = tmp_path / 'in_0.yaml'
+    with infilename.open('w') as infile:
+        infile.write(yaml_0)
+
+    auxfilename = tmp_path / 'aux.yaml'
+    with auxfilename.open('w') as auxfile:
+        auxfile.write(yaml_1)
+
     # Run preprocessor
     preprocessor = DataPreProcessor()
-    keymap = {"$FILE_PATH": tmp_path}
+    keymap = {"FILE_PATH": str(tmp_path)}
     preprocessor.add_replacements_map(keymap)
-    preprocessor.process_yaml(tmp_path + "/a.yaml", tmp_path + "/test_0.yaml")
+    outfilename = tmp_path / 'test_0.yaml'
+    preprocessor.process_yaml(tmp_path / 'in_0.yaml', outfilename)
     # Check output
-    compare_files(tmp_path + "/test_0.yaml", tmp_path + "/reference_0.yaml")
+    ref_yaml = yaml.load(reference)
+    assert yaml.load(outfilename.open()) == ref_yaml
